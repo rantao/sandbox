2b075e0afaa5f86685d7413447f330885fda84b6
Ran Tao <ran@ephexi.com>
2012-04-25 12:37:53 -0700

    Initial Commit

---
+++ SandboxTests/SandboxTests.h
@@ -0,0 +1,13 @@
+//
+//  SandboxTests.h
+//  SandboxTests
+//
+//  Created by Ran Tao on 4.25.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SandboxTests : SenTestCase
+
+@end
---
+++ SandboxTests/SandboxTests.m
@@ -0,0 +1,32 @@
+//
+//  SandboxTests.m
+//  SandboxTests
+//
+//  Created by Ran Tao on 4.25.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SandboxTests.h"
+
+@implementation SandboxTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SandboxTests");
+}
+
+@end
